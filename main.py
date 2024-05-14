8586f536c21b47c0e78ea47ef080cae199bc4602
g-vinokurov <g.vinokurov@g.nsu.ru>
2024-05-14 16:05:40 +0700

    Add Auth REST API

--- main.py
+++ main.py
@@ -3,15 +3,32 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-
-class Item(BaseModel):
-    item_id: int
-    q: Union[str, None] = None
-
-
 app = FastAPI()
 
 
-@app.get('/items/{item_id}', response_model=Item)
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {'item_id': item_id, 'q': q}
+class RegisterRequest(BaseModel):
+    email: str
+    password: str
+
+
+class RegisterResponse(BaseModel):
+    user_id: int
+
+
+class LoginRequest(BaseModel):
+    email: str
+    password: str
+
+
+class LoginResponse(BaseModel):
+    user_id: int
+
+
+@app.post('/api/auth/register', response_model=RegisterResponse)
+def api_auth_register(request: RegisterRequest):
+    return {'user_id': 0}
+
+
+@app.post('/api/auth/login', response_model=LoginResponse)
+def api_auth_login(request: LoginRequest):
+    return {'user_id': 0}
