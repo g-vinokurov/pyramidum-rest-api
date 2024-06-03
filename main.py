ae86547a50da9a6828e00390a913d01bf561fad1
g-vinokurov <g.vinokurov@g.nsu.ru>
2024-06-03 11:15:37 +0700

    Update contract

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 from enum import Enum
+from datetime import date
 from datetime import datetime
 
 from uuid import UUID
@@ -40,17 +41,91 @@ class LoginResponse(BaseModel):
     user_id: int
 
 
-# 1715678526
-# class Motivator(BaseModel):
-#     id: UUID
-#     user_id: int
-#     start_time: datetime
-#     end_time: datetime = None
-#
-#     @field_validator('start_time', 'end_time')
-#     @classmethod
-#     def dt_validate(cls, dt):
-#         return datetime.fromtimestamp(dt) if dt is not None else None
+class ProgressStatus(Enum):
+    PROGRESS_STATUS_CANCELED = 0
+    PROGRESS_STATUS_IN_PROGRESS = 1
+    PROGRESS_STATUS_DONE = 2
+
+
+class Task(BaseModel):
+    id: UUID
+    header: str
+    text: str
+    external_images: list[str]
+    deadline: date
+    progress_status: ProgressStatus
+    is_urgent: bool
+    is_important: bool
+    owner_id: int
+    parent_id: UUID | None = None
+    possible_deadline: date
+    weight: int
+
+
+class CreateTaskRequest(BaseModel):
+    header: str
+    text: str
+    external_images: list[str]
+    deadline: date
+    progress_status: ProgressStatus
+    is_urgent: bool
+    is_important: bool
+    owner_id: int
+    parent_id: UUID | None = None
+    possible_deadline: date
+    weight: int
+
+
+class CreateTaskResponse(BaseModel):
+    task_id: UUID
+
+
+class UpdateTaskRequest(BaseModel):
+    task_id: UUID
+
+
+class UpdateTaskRequestBody(BaseModel):
+    header: str
+    text: str
+    external_images: list[str]
+    deadline: date
+    progress_status: ProgressStatus
+    is_urgent: bool
+    is_important: bool
+    owner_id: int
+    parent_id: UUID | None = None
+    possible_deadline: date
+    weight: int
+
+
+class UpdateTaskResponse(BaseModel):
+    task_id: UUID
+
+
+class TaskRequest(BaseModel):
+    task_id: UUID
+
+
+class TaskResponse(Task):
+    pass
+
+
+class TasksRequest(BaseModel):
+    user_id: int
+    search: str | None = None
+    deadline_from: date | None = None
+    deadline_to: date | None = None
+    possible_deadline_from: date | None = None
+    possible_deadline_to: date | None = None
+    progress_status: ProgressStatus | None = None
+    is_urgent: bool | None = None
+    is_important: bool | None = None
+    weight_from: int | None = None
+    weight_to: int | None = None
+
+
+class TasksResponse(BaseModel):
+    tasks: list[Task]
 
 
 class StartSessionRequest(BaseModel):
@@ -69,69 +144,6 @@ class StopSessionResponse(BaseModel):
     id: UUID
 
 
-class ProgressStatus(Enum):
-    PROGRESS_STATUS_CANCELED = 0
-    PROGRESS_STATUS_IN_PROGRESS = 1
-    PROGRESS_STATUS_DONE = 2
-
-
-class Task(BaseModel):
-    id: UUID
-    header: str
-    text: str
-    external_images: list[str]
-    deadline: int
-    progress_status: ProgressStatus
-    is_urgent: bool
-    is_important: bool
-    owner_id: int
-    parent_id: UUID | None = None
-    possible_deadline: int
-    weight: int
-
-
-class CreateTaskRequest(BaseModel):
-    header: str
-    text: str
-    external_images: list[str]
-    deadline: int
-    progress_status: ProgressStatus
-    is_urgent: bool
-    is_important: bool
-    owner_id: int
-    parent_id: UUID | None = None
-    possible_deadline: int
-    weight: int
-
-
-class CreateTaskResponse(BaseModel):
-    task_id: UUID
-
-
-class UpdateTaskRequest(BaseModel):
-    task: Task
-
-
-class UpdateTaskResponse(BaseModel):
-    task_id: UUID
-
-
-class GetTaskRequest(BaseModel):
-    task_id: UUID
-
-
-class GetTaskResponse(BaseModel):
-    task: Task
-
-
-class GetTaskByUserIDRequest(BaseModel):
-    user_id: int
-
-
-class GetTaskByUserIDResponse(BaseModel):
-    tasks: list[Task]
-
-
 @app.post('/api/auth/register', response_model=RegisterResponse)
 def api_auth_register(request: RegisterRequest):
     return {'user_id': 0}
@@ -142,14 +154,24 @@ def api_auth_login(request: LoginRequest):
     return {'user_id': 0}
 
 
-@app.get('/api/motivator/start-session', response_model=StartSessionResponse)
-def api_motivator_start_session(user_id: int):
-    return {'id': uuid.uuid5(uuid.NAMESPACE_DNS, 'api-motivator-start-session')}
-
-
-@app.get('/api/motivator/stop-session/{session_uuid}', response_model=StopSessionResponse)
-def api_motivator_stop_session(session_uuid: UUID):
-    return {'id': session_uuid}
+@app.get('/api/tasks', response_model=TasksResponse)
+def api_tasks_get_all(request: TasksRequest):
+    return [
+        {
+            'id': uuid.uuid5(uuid.NAMESPACE_DNS, 'api-tasks-get-by-user-id'),
+            'header': 'Header',
+            'text': 'Text',
+            'external_images': [],
+            'deadline': datetime.utcnow().date(),
+            'progress_status': ProgressStatus.PROGRESS_STATUS_IN_PROGRESS,
+            'is_urgen': True,
+            'is_important': False,
+            'owner_id': 0,
+            'parent_id': uuid.uuid5(uuid.NAMESPACE_DNS, 'api-tasks-get-by-user-id-parent'),
+            'possible_deadline': datetime.utcnow().date(),
+            'weight': 0
+        },
+    ]
 
 
 @app.post('/api/tasks', response_model=CreateTaskResponse)
@@ -157,44 +179,24 @@ def api_tasks_create(request: CreateTaskRequest):
     return {'task_id': uuid.uuid5(uuid.NAMESPACE_DNS, 'api-tasks-create')}
 
 
-@app.put('/api/tasks', response_model=UpdateTaskResponse)
-def api_tasks_update(request: UpdateTaskRequest):
+@app.put('/api/tasks/{task_id}', response_model=UpdateTaskResponse)
+def api_tasks_update(task_id: UUID, request: UpdateTaskRequestBody):
     return {'task_id': uuid.uuid5(uuid.NAMESPACE_DNS, 'api-tasks-update')}
 
 
-@app.get('/api/tasks/{task_id}', response_model=GetTaskResponse)
-def api_tasks_get(task_id: UUID):
+@app.get('/api/tasks/{task_id}', response_model=TaskResponse)
+def api_tasks_get_one(task_id: UUID):
     return {
         'id': uuid.uuid5(uuid.NAMESPACE_DNS, 'api-tasks-get-one'),
         'header': 'Header',
         'text': 'Text',
         'external_images': [],
-        'deadline': datetime.utcnow().timestamp(),
+        'deadline': datetime.utcnow().date(),
         'progress_status': ProgressStatus.PROGRESS_STATUS_IN_PROGRESS,
         'is_urgen': True,
         'is_important': False,
         'owner_id': 0,
         'parent_id': uuid.uuid5(uuid.NAMESPACE_DNS, 'api-tasks-get-one-parent'),
-        'possible_deadline': datetime.utcnow().timestamp(),
+        'possible_deadline': datetime.utcnow().date(),
         'weight': 0
     }
-
-
-@app.get('/api/tasks', response_model=GetTaskByUserIDResponse)
-def api_tasks_get_by_user_id(user_id: int):
-    return [
-        {
-            'id': uuid.uuid5(uuid.NAMESPACE_DNS, 'api-tasks-get-by-user-id'),
-            'header': 'Header',
-            'text': 'Text',
-            'external_images': [],
-            'deadline': datetime.utcnow().timestamp(),
-            'progress_status': ProgressStatus.PROGRESS_STATUS_IN_PROGRESS,
-            'is_urgen': True,
-            'is_important': False,
-            'owner_id': 0,
-            'parent_id': uuid.uuid5(uuid.NAMESPACE_DNS, 'api-tasks-get-by-user-id-parent'),
-            'possible_deadline': datetime.utcnow().timestamp(),
-            'weight': 0
-        },
-    ]
